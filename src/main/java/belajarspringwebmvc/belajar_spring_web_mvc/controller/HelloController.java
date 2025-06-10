e9612bce50920e0f45f96affd8602e06f27fea6b
Karungg <miftahfadilah71@gmail.com>
2025-06-11 06:21:21 +0700

    Request mapping

--- src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/HelloController.java
+++ src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/HelloController.java
@@ -1,8 +1,18 @@
 package belajarspringwebmvc.belajar_spring_web_mvc.controller;
 
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloController {
+
+    @RequestMapping("/hello")
+    public void helloWorld(HttpServletResponse response) throws IOException {
+        response.getWriter().println("Hello world");
+    }
 
 }
