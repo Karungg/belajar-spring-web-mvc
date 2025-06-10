5ee74d7980f1d46ea8e5c928a43196a1100549fc
Karungg <miftahfadilah71@gmail.com>
2025-06-11 06:13:53 +0700

    Initial commit

---
+++ src/test/java/belajarspringwebmvc/belajar_spring_web_mvc/BelajarSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package belajarspringwebmvc.belajar_spring_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
