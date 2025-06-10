05b347ad210fd5772362512178913487f21c63d5
Karungg <miftahfadilah71@gmail.com>
2025-06-11 06:25:22 +0700

    Servlet request and response

--- src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/HelloController.java
+++ src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/HelloController.java
@@ -1,18 +1,26 @@
 package belajarspringwebmvc.belajar_spring_web_mvc.controller;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloController {
 
     @RequestMapping("/hello")
-    public void helloWorld(HttpServletResponse response) throws IOException {
-        response.getWriter().println("Hello world");
+    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+
+        if (Objects.isNull(name)) {
+            name = "Guest";
+        }
+
+        response.getWriter().println("Hello " + name);
     }
 
 }
