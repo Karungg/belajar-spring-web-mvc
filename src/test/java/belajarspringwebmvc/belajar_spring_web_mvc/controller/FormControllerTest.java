5d8f18a81ed6327b7755bc212f93322a4025f188
Karungg <miftahfadilah71@gmail.com>
2025-06-11 14:43:31 +0700

    Request content type

---
+++ src/test/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/FormControllerTest.java
@@ -0,0 +1,34 @@
+package belajarspringwebmvc.belajar_spring_web_mvc.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FormControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHello() throws Exception {
+        mockMvc
+                .perform(
+                        post("/form/hello")
+                                .queryParam("name", "Miftah Fadilah")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpectAll(
+                        status().isOk(),
+                        content().string(Matchers.containsString("Hello Miftah Fadilah")));
+    }
+
+}
