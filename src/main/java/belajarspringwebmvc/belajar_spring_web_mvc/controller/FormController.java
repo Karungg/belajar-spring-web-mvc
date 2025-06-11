deb27d0d08502c6881dafce5c9feb7b827bd4d63
Karungg <miftahfadilah71@gmail.com>
2025-06-11 19:40:53 +0700

    Form request

--- src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/FormController.java
+++ src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/FormController.java
@@ -1,5 +1,8 @@
 package belajarspringwebmvc.belajar_spring_web_mvc.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FormController {
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @PostMapping(path = "/form/hello", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
@@ -19,6 +24,17 @@ public class FormController {
                 </body>
                 </html>
                 """.replace("$name", name);
+    }
+
+    @PostMapping(path = "/form/person", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseBody
+    public String createPerson(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "date") Date date,
+            @RequestParam(name = "address") String address) {
+
+        return "Create person successfully! Name : " + name + ", Date of birth : " + dateFormat.format(date)
+                + ", Address : " + address;
     }
 
 }
--- src/test/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/FormControllerTest.java
+++ src/test/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/FormControllerTest.java
@@ -18,20 +18,37 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class FormControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Test
-    void testHello() throws Exception {
-        mockMvc
-                .perform(
-                        post("/form/hello")
-                                .queryParam("name", "Miftah Fadilah")
-                                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
-                .andExpectAll(
-                        status().isOk(),
-                        header().string(HttpHeaders.CONTENT_TYPE, Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
-                        content().string(Matchers.containsString("Hello Miftah Fadilah")));
-    }
+        @Test
+        void testHello() throws Exception {
+                mockMvc
+                                .perform(
+                                                post("/form/hello")
+                                                                .queryParam("name", "Miftah Fadilah")
+                                                                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                                .andExpectAll(
+                                                status().isOk(),
+                                                header().string(HttpHeaders.CONTENT_TYPE,
+                                                                Matchers.containsString(MediaType.TEXT_HTML_VALUE)),
+                                                content().string(Matchers.containsString("Hello Miftah Fadilah")));
+        }
+
+        @Test
+        void testCreatePerson() throws Exception {
+                mockMvc.perform(
+                                post("/form/person")
+                                                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                                                .param("name", "Miftah Fadilah")
+                                                .param("date", "2004-11-27")
+                                                .param("address", "Indonesia"))
+                                .andExpectAll(
+                                                status().isOk(),
+                                                content().string(Matchers.containsString(
+                                                                "Create person successfully! Name : Miftah Fadilah, Date of birth : 2004-11-27, Address : Indonesia")),
+                                                header().string(HttpHeaders.CONTENT_TYPE, Matchers.containsString(
+                                                                MediaType.TEXT_PLAIN_VALUE)));
+        }
 
 }
