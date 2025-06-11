44438e1bb4a5758d8f09ef38e82bb9191179ba00
Karungg <miftahfadilah71@gmail.com>
2025-06-11 14:30:41 +0700

    Response body

--- src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/DateController.java
+++ src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/DateController.java
@@ -1,14 +1,12 @@
 package belajarspringwebmvc.belajar_spring_web_mvc.controller;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DateController {
@@ -16,8 +14,9 @@ public class DateController {
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
 
     @GetMapping(path = "/date")
-    public void getDate(@RequestParam(name = "date") Date date, HttpServletResponse response) throws IOException {
-        response.getWriter().println("Date : " + dateFormat.format(date));
+    @ResponseBody
+    public String getDate(@RequestParam(name = "date") Date date) {
+        return "Date : " + dateFormat.format(date);
     }
 
 }
