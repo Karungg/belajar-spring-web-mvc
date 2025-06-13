0b0e5415ed2ef793cce8a01ed4baa8eed1a17549
Karungg <miftahfadilah71@gmail.com>
2025-06-13 14:37:09 +0700

    Mvc config

---
+++ src/main/java/belajarspringwebmvc/belajar_spring_web_mvc/config/MyWebConfig.java
@@ -0,0 +1,9 @@
+package belajarspringwebmvc.belajar_spring_web_mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+
+}
