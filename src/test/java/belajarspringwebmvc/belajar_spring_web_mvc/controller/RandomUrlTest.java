c4d9f3d0f591de9fbc733f4f56636b938c86019b
Karungg <miftahfadilah71@gmail.com>
2025-06-13 18:21:18 +0700

    Static page

---
+++ src/test/java/belajarspringwebmvc/belajar_spring_web_mvc/controller/RandomUrlTest.java
@@ -0,0 +1,29 @@
+package belajarspringwebmvc.belajar_spring_web_mvc.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RandomUrlTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testIndex() throws Exception {
+        mockMvc.perform(
+                get("/index.html")).andExpectAll(
+                        status().isOk(),
+                        content().string(Matchers.containsString("Hello static")));
+    }
+
+}
